ccf95ec8fed59c72dfeb3df88f88f3be54b743e8
egorlox1415 <no231722@gmail.com>
2024-06-03 13:46:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Hi Welcome to this world,you dead")
+print("create your personal people")
+name = input("your name")
+print("your name:",name)
+gender = input("what your gender:")
+print("your gender",gender)
+race = input("what your race ")
+print("your race", race)
+print("*" * 50)
+print("you 💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩")
+print("in front of you be 3 place:park bar moon?")
+place =input("where will you go?")
+if place =="bar":
+ print("vam dal🐞i po litsuy")
+
+
+elif place.lower() == "moon":
+    print("vi ymerli iz za nedostatka vozdyxa")
+
+elif place.lower() == "park":
+    print("vi vigili")
+
+
+elif place.lower() == "академия":
+    print("очень весело потом смерть ")
+
+else:
+    print("takoi loki nety , ti lox")
